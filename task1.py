ea2cfb1d6b54fb38def88bef6419478a9cb056b3
thorabdoo756-ui <thor.abdoo756@students.cmcss.net>
2025-12-17 08:38:47 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,25 @@
+# Halloween Candy Sharing
 
+# Given Variables
+people = 2  # friends who went trick-or-treating
+bagA = 23
+bagB = 17
+bagC = 19
+
+# Part 1: Combine the haul
+total_candy = bagA + bagB + bagC
+print("Total candy collected:", total_candy)
+
+# Part 2: Fair sharing (include yourself)
+people = people + 1  # include yourself
+share = total_candy // people
+leftover = total_candy % people
+print("Each person gets:", share)
+print("Leftover candy:", leftover)
+
+# Part 3: Include the sick friend
+people = people + 1  # include sick friend
+share = total_candy // people
+leftover = total_candy % people
+print("Each person gets:", share)
+print("Leftover candy:", leftover)
